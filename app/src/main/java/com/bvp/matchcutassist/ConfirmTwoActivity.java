6873f4584073874309a25a3b272e2a1bec117a32
Dell <bparekh@cpp.edu>
2019-07-25 21:51:26 -0700

    share image file

--- app/src/main/java/com/bvp/matchcutassist/ConfirmTwoActivity.java
+++ app/src/main/java/com/bvp/matchcutassist/ConfirmTwoActivity.java
@@ -3,24 +3,26 @@ package com.bvp.matchcutassist;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class ConfirmTwoActivity extends AppCompatActivity {
+
+    File oneImage = CameraOneActivity.getOutputMediaFile();
+    File twoImage = CameraTwoActivity.getOutputMediaFile();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_two);
-
-        File oneImage = CameraOneActivity.getOutputMediaFile();
-        File twoImage = CameraTwoActivity.getOutputMediaFile();
 
         //Showing both the images on a page
         if(oneImage.exists() && twoImage.exists()){
@@ -41,10 +43,21 @@ public class ConfirmTwoActivity extends AppCompatActivity {
         finish();
     }
 
-    public void share(View v){
-        /*Intent cameraTwoIntent = new Intent(getApplicationContext(), CameraTwoActivity.class);
-        startActivity(cameraTwoIntent);*/
+    public void share(View v) {
+
+        ArrayList<Uri> imageUris = new ArrayList();
+
+        imageUris.add(FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID, oneImage));
+        imageUris.add(FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID, twoImage));
+
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
+        shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris);
+        shareIntent.setType("image/*");
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(Intent.createChooser(shareIntent, "Share images via"));
+
         Toast.makeText(getApplicationContext(), "Exiting application.", Toast.LENGTH_LONG).show();
-        finish();
+        //finish();
     }
 }
